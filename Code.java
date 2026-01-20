407c5d36bf75e976cd1fd31c487f6248339f07e6
Elijah Budd <eabudd@students.unwsp.edu>
2026-01-20 16:08:40 -0600

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,1 +1,13 @@
+package standard;
 
+public class Program_1_Display_APP {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Two things are infinite: the universe and human stupidity;\n"); 
+		System.out.format("%50s","and I'm not sure about the universe.\n");
+		System.out.print("\t-Albert Einstein");	
+
+	}
+
+}
